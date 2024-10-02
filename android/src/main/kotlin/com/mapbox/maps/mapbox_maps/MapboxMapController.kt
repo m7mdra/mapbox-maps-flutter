444414194f5dacf86394d6faf08f1d6046a266f4
Mohamed Elshiekh <xm7mdrax@gmail.com>
2024-10-02 15:54:18 +0300

    fix issue with lifecycle plugin

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/MapboxMapController.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/MapboxMapController.kt
@@ -3,9 +3,19 @@ package com.mapbox.maps.mapbox_maps
 import android.content.Context
 import android.view.View
 import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import com.mapbox.common.*
-import com.mapbox.maps.*
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.setViewTreeLifecycleOwner
+import com.mapbox.common.DownloadOptions
+import com.mapbox.common.HttpHeaders
+import com.mapbox.common.HttpRequest
+import com.mapbox.common.HttpResponse
+import com.mapbox.common.HttpServiceFactory
+import com.mapbox.common.HttpServiceInterceptorInterface
+import com.mapbox.maps.MapInitOptions
+import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.mapbox_maps.annotation.AnnotationController
 import com.mapbox.maps.pigeons.FLTMapInterfaces
 import com.mapbox.maps.pigeons.FLTSettings
@@ -13,11 +23,12 @@ import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.platform.PlatformView
+import kotlin.collections.set
 
 class MapboxMapController(
   context: Context,
   mapInitOptions: MapInitOptions,
-  private val lifecycleProvider: MapboxMapsPlugin.LifecycleProvider,
+  lifecycleProvider: MapboxMapsPlugin.LifecycleProvider,
   eventTypes: List<String>,
   messenger: BinaryMessenger,
   channelSuffix: Int,
@@ -26,27 +37,49 @@ class MapboxMapController(
   DefaultLifecycleObserver,
   MethodChannel.MethodCallHandler {
 
-  private val mapView: MapView = MapView(context, mapInitOptions)
-  private val mapboxMap: MapboxMap = mapView.getMapboxMap()
+  private var mapView: MapView? = null
+  private var mapboxMap: MapboxMap? = null
   private val methodChannel: MethodChannel
-  private val styleController: StyleController = StyleController(mapboxMap)
-  private val cameraController: CameraController = CameraController(mapboxMap)
-  private val projectionController: MapProjectionController = MapProjectionController(mapboxMap)
-  private val mapInterfaceController: MapInterfaceController = MapInterfaceController(mapboxMap)
-  private val animationController: AnimationController = AnimationController(mapboxMap)
-  private val annotationController: AnnotationController = AnnotationController(mapView, mapboxMap)
-  private val locationComponentController = LocationComponentController(mapView)
-  private val gestureController = GestureController(mapView)
-  private val logoController = LogoController(mapView)
-  private val attributionController = AttributionController(mapView)
-  private val scaleBarController = ScaleBarController(mapView)
-  private val compassController = CompassController(mapView)
+  private val styleController: StyleController
+  private val cameraController: CameraController
+  private val projectionController: MapProjectionController
+  private val mapInterfaceController: MapInterfaceController
+  private val animationController: AnimationController
+  private val annotationController: AnnotationController
+  private val locationComponentController: LocationComponentController
+  private val gestureController: GestureController
+  private val logoController: LogoController
+  private val attributionController: AttributionController
+  private val scaleBarController: ScaleBarController
+  private val compassController: CompassController
 
   private val proxyBinaryMessenger = ProxyBinaryMessenger(messenger, "/map_$channelSuffix")
 
+  private val lifecycleHelper: LifecycleHelper
+
   init {
+    val mapView = MapView(context, mapInitOptions)
+    val mapboxMap = mapView.getMapboxMap()
+    this.mapView = mapView
+    this.mapboxMap = mapboxMap
+    styleController = StyleController(mapboxMap)
+    cameraController = CameraController(mapboxMap)
+    projectionController = MapProjectionController(mapboxMap)
+    mapInterfaceController = MapInterfaceController(mapboxMap)
+    animationController = AnimationController(mapboxMap)
+    annotationController = AnnotationController(mapView, mapboxMap)
+    locationComponentController = LocationComponentController(mapView)
+    gestureController = GestureController(mapView)
+    logoController = LogoController(mapView)
+    attributionController = AttributionController(mapView)
+    scaleBarController = ScaleBarController(mapView)
+    compassController = CompassController(mapView)
+
     changeUserAgent(pluginVersion)
-    lifecycleProvider.getLifecycle()?.addObserver(this)
+
+    lifecycleHelper = LifecycleHelper(lifecycleProvider.getLifecycle()!!)
+    mapView.setViewTreeLifecycleOwner(lifecycleHelper)
+
     FLTMapInterfaces.StyleManager.setup(proxyBinaryMessenger, styleController)
     FLTMapInterfaces._CameraManager.setup(proxyBinaryMessenger, cameraController)
     FLTMapInterfaces.Projection.setup(proxyBinaryMessenger, projectionController)
@@ -69,14 +102,17 @@ class MapboxMapController(
     )
   }
 
-  override fun getView(): View {
+  override fun getView(): View? {
     return mapView
   }
 
   override fun dispose() {
-    lifecycleProvider.getLifecycle()?.removeObserver(this)
-    mapView.onStop()
-    mapView.onDestroy()
+    if (mapView == null) {
+      return
+    }
+    lifecycleHelper.dispose()
+    mapView = null
+    mapboxMap = null
     methodChannel.setMethodCallHandler(null)
     FLTMapInterfaces.StyleManager.setup(proxyBinaryMessenger, null)
     FLTMapInterfaces._CameraManager.setup(proxyBinaryMessenger, null)
@@ -92,21 +128,12 @@ class MapboxMapController(
     FLTSettings.AttributionSettingsInterface.setup(proxyBinaryMessenger, null)
   }
 
-  override fun onStart(owner: LifecycleOwner) {
-    super.onStart(owner)
-    mapView.onStart()
-  }
-
-  override fun onStop(owner: LifecycleOwner) {
-    super.onStop(owner)
-    mapView.onStop()
-  }
-
   override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
     when (call.method) {
       "map#subscribe" -> {
         val eventType = call.argument<String>("event")!!
-        mapboxMap.subscribe(
+        println(eventType)
+        mapboxMap?.subscribe(
           { event ->
             methodChannel.invokeMethod(getEventMethodName(eventType), event.data.toJson())
           },
@@ -114,20 +141,25 @@ class MapboxMapController(
         )
         result.success(null)
       }
+
       "annotation#create_manager" -> {
         annotationController.handleCreateManager(call, result)
       }
+
       "annotation#remove_manager" -> {
         annotationController.handleRemoveManager(call, result)
       }
+
       "gesture#add_listeners" -> {
         gestureController.addListeners(proxyBinaryMessenger)
         result.success(null)
       }
+
       "gesture#remove_listeners" -> {
         gestureController.removeListeners()
         result.success(null)
       }
+
       else -> {
         result.notImplemented()
       }
@@ -138,7 +170,8 @@ class MapboxMapController(
     HttpServiceFactory.getInstance().setInterceptor(
       object : HttpServiceInterceptorInterface {
         override fun onRequest(request: HttpRequest): HttpRequest {
-          request.headers[HttpHeaders.USER_AGENT] = "${request.headers[HttpHeaders.USER_AGENT]} Flutter Plugin/$version"
+          request.headers[HttpHeaders.USER_AGENT] =
+            "${request.headers[HttpHeaders.USER_AGENT]} Flutter Plugin/$version"
           return request
         }
 
@@ -154,4 +187,52 @@ class MapboxMapController(
   }
 
   private fun getEventMethodName(eventType: String) = "event#$eventType"
+
+  /*
+    Mirrors lifecycle of the parent, with one addition - switches to DESTROYED state
+    when `dispose` is called.
+    `parentLifecycle` is the lifecycle of the Flutter activity, which may live much longer then
+    the MapView attached.
+  */
+  private class LifecycleHelper(override val lifecycle: Lifecycle) : LifecycleOwner, DefaultLifecycleObserver {
+
+    val lifecycleRegistry: LifecycleRegistry = LifecycleRegistry(this)
+
+    init {
+      lifecycle.addObserver(this)
+    }
+
+    override fun onCreate(owner: LifecycleOwner) {
+      lifecycleRegistry.currentState = Lifecycle.State.CREATED
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+      lifecycleRegistry.currentState = Lifecycle.State.STARTED
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+      lifecycleRegistry.currentState = Lifecycle.State.RESUMED
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+      lifecycleRegistry.currentState = Lifecycle.State.STARTED
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+      lifecycleRegistry.currentState = Lifecycle.State.CREATED
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+      lifecycleRegistry.currentState = Lifecycle.State.DESTROYED
+    }
+
+    fun dispose() {
+      lifecycle.removeObserver(this)
+      // fires MapView.onStop
+      lifecycleRegistry.currentState = Lifecycle.State.CREATED
+      // fires MapView.onDestroy
+      lifecycleRegistry.currentState = Lifecycle.State.DESTROYED
+    }
+  }
+
 }
